cbd75d4892fae0c030ce8f1d23a062738e2a1d70
Serhii269 <podgorodeckojsega@gmail.com>
2025-08-28 23:16:54 +0200

    added contacts section and footer

---
+++ js/main.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('header a[href^="#"]').forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  });
+});
